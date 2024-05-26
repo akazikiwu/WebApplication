f8d4aa37e0218bc58d888537d53af48c3b14e5b4
akaziki <874601941@qq.com>
2024-05-26 19:53:15 +0800

    项目创建

---
+++ src/main/java/com/fourteen/wms/WmsApplication.java
@@ -0,0 +1,13 @@
+package com.fourteen.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsApplication.class, args);
+    }
+
+}
